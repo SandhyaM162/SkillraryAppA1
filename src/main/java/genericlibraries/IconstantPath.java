2399036261d819837eebf219f3379c41f8824f92
sandhyashiva <sandhyashiva@SandhyaShiva>
2023-11-08 12:25:15 +0530

    COMMIT MESSAGE

---
+++ src/main/java/genericlibraries/IconstantPath.java
@@ -0,0 +1,9 @@
+package genericlibraries;
+
+public class IconstantPath {
+
+	
+	public static String PROPERTIES_PATH="./src/test/resources/commonData.properties";
+	public static String EXCEL_PATH="./src/test/resources/Book1excelS.xlsx";
+
+}
